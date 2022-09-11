f588be82c399f6dbf3208511efe63e6cde9feba5
Yang Zhao <freda.y.zhao@gmail.com>
2022-09-11 16:32:07 -0400

    update

---
+++ class-2_assignment 2/js/scripts.js
@@ -0,0 +1,75 @@
+//1. Conditions
+let num = Math.floor(Math.random() * 100);
+
+const checkOdd = (num) => {
+  num % 2 === 0
+    ? console.log(`${num} is even.`)
+    : console.log(`${num} is odd.`);
+};
+
+checkOdd(num);
+
+//2. Conditions/Random
+let rolled = Math.ceil(Math.random() * 6);
+
+const rollDice = (rolled) => {
+  switch (rolled) {
+    case 1:
+      console.log("You rolled a one.");
+      break;
+    case 2:
+      console.log("You rolled a two.");
+      break;
+    case 3:
+      console.log("You rolled a three.");
+      break;
+    case 4:
+      console.log("You rolled a four.");
+      break;
+    case 5:
+      console.log("You rolled a five.");
+      break;
+    case 6:
+      console.log("You rolled a six.");
+      break;
+    default:
+      console.log("You rolled a number that does not exist on a dice.");
+  }
+};
+
+rollDice(rolled);
+
+//3. Loops
+// let rolled2 = Math.ceil(Math.random() * 6);
+// while (rolled2 <= 3) {
+//   console.log(`The loop ran ${rolled2}time(s).`);
+// }
+
+//4. Loops
+let num2 = Math.floor(Math.random() * 10);
+let fact = 1;
+for (let i = num2; i > 0; i--) {
+  fact *= i;
+}
+console.log(`${num2}'s factorial is ${fact}`);
+
+//5. Loops
+let pound = "";
+for (let i = 0; i < 4; i++) {
+  pound += "#";
+  console.log(pound);
+}
+
+//6. Loops and Conditions
+
+let board = "";
+for (let col = 0; col < 8; col++) {
+  for (let row = 0; row < 8; row++) {
+    if ((col + row) % 2 === 0) {
+      board += "#";
+    } else board += " ";
+  }
+
+  // board += "\n";
+}
+console.log(board);
--- class-2_html-boilerplate copy/js/scripts.js
+++ class-2_html-boilerplate copy/js/scripts.js
@@ -1,3 +1,23 @@
+/* always add comments
+for multi line use */
+
+//***********Class Notes */
+// let a = 2;
+// let b = 46;
+// b = b % a;
+
+// const my_array = [1, 2, 3];
+// my_array[0] = 2;
+// console.log(my_array);
+
+// let num = Math.floor(Math.random()) * 100;
+// num % 2 === 0 ? console.log("number is even") : console.log("number is odd");
+
+// //roll the dice
+
+// let num2 = Math.ceil(Math.random() * 6);
+// console.log(num2);
+
 let today_day = prompt("What is today?");
 if (today_day.toLowerCase() === "thursday") {
   let check_empty = prompt("Is there any spot?");
