43793f18799e09d5227223bb527bb3ae26cf101b
Yang Zhao <freda.y.zhao@gmail.com>
2022-10-02 17:37:52 -0400

    update assignment 5

--- class-5-assignment/js/scripts.js
+++ class-5-assignment/js/scripts.js
@@ -15,23 +15,40 @@ section.insertBefore(new_para, secondPara);
 
 //3. Write code that inserts an image and inserts it inside the `<section>` tag.
 
-/* 3a. Start from 3 and now write code that, on click of a button ('click' eventListener), can randomly fetch an image from that url and insert it somewhere inside the body. To make it more fun, try to assign it as a background image to the whole body (make it cover fit). Then update your code so that the image can change every time you click the button. 
-
-4.  Extend the existing table with a few additional rows (tr and td) in the html. Then write code to select the table rows and build a loop to assign alternative colors to the rows.
-*/
-const image = document.createElement("div");
-section.appendChild(div);
-image.style.backgroundImage = "url('https://source.unsplash.com/random')";
+// 3a. Start from 3 and now write code that, on click of a button ('click' eventListener), can randomly fetch an image from that url and insert it somewhere inside the body. To make it more fun, try to assign it as a background image to the whole body (make it cover fit). Then update your code so that the image can change every time you click the button.
 
 const button = document.createElement("button");
 button.innerHTML = "click me";
 section.appendChild(button);
 
-// button.addEventListener("click", showImage);
+let counter = 1;
+button.addEventListener("click", showImage);
 
-// function showImage() {
-//   const image = document.createElement("img");
-//   image.setAttribute("src", "https://source.unsplash.com/random");
-//   image.style.width = "400px";
-//   section.appendChild(image);
-// }
+//I couldn't get the image to change again
+function showImage() {
+  document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/random')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "cover";
+}
+
+// 4.  Extend the existing table with a few additional rows (tr and td) in the html. Then write code to select the table rows and build a loop to assign alternative colors to the rows.
+
+for (let i = 2; i < 5; i++) {
+  for (let j = 1; j < 2; j++) {
+    const tbody = document.querySelector("tbody");
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>Row ${i} <b>Cell ${j}</b></td><td>Row ${i} <b>Cell ${
+      j + 1
+    }</b></td>`;
+    tbody.appendChild(tr);
+  }
+}
+
+const trs = document.querySelectorAll("tr");
+
+for (let i = 0; i < trs.length; i++) {
+  if (i % 2 === 0) {
+    trs[i].style.backgroundColor = "cornflowerblue";
+  }
+}
